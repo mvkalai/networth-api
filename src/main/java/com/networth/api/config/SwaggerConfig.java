5e046e63d255c09b526aef304797739ab9ababd5
kalai <mvk.kalai@gmail.com>
2021-12-29 09:13:08 +0530

    Swagger configuration

--- src/main/java/com/networth/api/config/SwaggerConfig.java
+++ src/main/java/com/networth/api/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.networth.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,16 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.networth.api.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("NetWorth - REST API Document")
+                .description("Help consumers of the NetWorth API know which all APIs are available, the signatures, the expected input. Also there should be some simple way to test if the API is up")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
                 .build();
     }
 
